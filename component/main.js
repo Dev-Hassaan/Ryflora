2c68b22947be24f95621bfbbf8b87cc515d052b2
Hassaan <syedhassaan720@gmail.com>
2024-09-08 17:54:48 +0500

    first commit

---
+++ component/main.js
@@ -0,0 +1,4 @@
+$(".loader").load("component/loader.html")
+$(".top-header-area").load("component/header.html")
+$(".footer-area").load("component/footer.html")
+$(".copyright").load("component/copyright.html")
